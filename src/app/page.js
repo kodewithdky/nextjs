5c823bd41df151bae4cd12078b8d6bd2bb0fb034
ydvdharmendrakumar <ydvdharmendrakumar@gmail.com>
2023-10-05 01:54:57 -0500

    css module with nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import styles from "./page.module.css";
+import outside from '../../src/app/style/outside.module.css'
 export default function Home() {
 
   const Compo = () => {
@@ -8,8 +9,8 @@ export default function Home() {
   };
   return (
     <main className={styles.main}>
-      <h1>Fetch data with api in client Component</h1>
-      <Link href="/productlist">Go to Product list</Link>
+      <h1 className="main">Fetch data with api in client Component</h1>
+      <Link href="/productlist" className={outside.main}>Go to Product list</Link>
     </main>
   );
 }
--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,4 +1,6 @@
 import Product from "./product";
+import costom from '../custom.module.css'
+import other from '../other.module.css'
 const getProduct = async () => {
   let data = await fetch("https://dummyjson.com/products");
   data = await data.json();
@@ -9,11 +11,11 @@ const Page = async () => {
   console.log(products);
   return (
     <div>
-      <h1>Product list</h1>
+      <h1 className={other.main}>Product list</h1>
        {
         products.map((item)=>(
             <div>
-            <h3>Name: {item.title}</h3>
+            <h3 className={costom.main}>Name: {item.title}</h3>
              <Product price={item.price}/>
             </div>
         ))
