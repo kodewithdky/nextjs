c1232498543149fbe849ea5ed84d70e7d1b0bf97
DK. Yadav <142752701+kodewithdky@users.noreply.github.com>
2023-10-11 12:39:45 -0500

    Update route.js , Removed database userid and password

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,7 +1,3 @@
 export async function GET(request) {
   return new Response("KodeWithDKY");
 }
-
-//mongodb+srv://ydvdharmendrakr:<password>@mycluster.tuhi9ac.mongodb.net/
-
-//password=BZt2np58z7mUR4vg
